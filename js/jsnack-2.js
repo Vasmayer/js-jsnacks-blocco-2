53a79fc349b2dd8449489f0ff02189165b02458e
Vasmayer <lealimarco90@gmail.com>
2022-01-13 16:44:18 +0100

    es js snack 2 complete whith BONUS

--- js/jsnack-2.js
+++ js/jsnack-2.js
@@ -5,6 +5,8 @@ se è dispari stampa il numero successivo
 
 */
 
+const jsnack2Element = document.getElementById('jsnack-2');
+let message = '';
 
 let isNumberValid = true;
 
@@ -15,10 +17,13 @@ do{
         if(!(inputNumber % 2))
         {
             console.log(inputNumber);
+            message = inputNumber
+       
         }
         else
         {
             console.log(inputNumber + 1);
+            message = ( inputNumber + 1 )
         }
         isNumberValid = true;
     }
@@ -28,3 +33,5 @@ do{
     }
 
 }while(!isNumberValid)
+
+jsnack2Element.innerHTML = 'Il numero Pari che hai inserito è:' + message ;
