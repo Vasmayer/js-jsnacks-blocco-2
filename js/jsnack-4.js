40328c106afd68738de3d42670e60a0a8c86c2f0
Vasmayer <lealimarco90@gmail.com>
2022-01-13 17:02:17 +0100

    es js snack 4 complete whith BONUS

--- js/jsnack-4.js
+++ js/jsnack-4.js
@@ -3,6 +3,8 @@ Crea un array di numeri interi
 e fai la somma di tutti gli elementi che sono in posizione dispari
 */
 
+const jsnack4Element = document.getElementById('jsnack-3');
+
 const wholeNumbers = [1,2,3,4,5,6,7,8,9,10];
 let sum = 0;
 
@@ -13,5 +15,7 @@ for(let i = 1; i < wholeNumbers.length ; i+=2)
 }
 
 console.log(sum);
+
+jsnack4Element.innerText = `La somma dei 10 numeri in posizione dispari è:${sum}`;
 
 
