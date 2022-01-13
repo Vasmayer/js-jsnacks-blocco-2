07da1ceb2e17108af567abd06429b13c7dc581c7
Vasmayer <lealimarco90@gmail.com>
2022-01-13 17:10:09 +0100

    es js snack 5 complete whith BONUS

--- js/jsnack-4.js
+++ js/jsnack-4.js
@@ -3,7 +3,7 @@ Crea un array di numeri interi
 e fai la somma di tutti gli elementi che sono in posizione dispari
 */
 
-const jsnack4Element = document.getElementById('jsnack-3');
+const jsnack4Element = document.getElementById('jsnack-4');
 
 const wholeNumbers = [1,2,3,4,5,6,7,8,9,10];
 let sum = 0;
--- js/jsnack-5.js
+++ js/jsnack-5.js
@@ -6,6 +6,8 @@ fino a quando ne avrà tanti quanti l’altro
 
 */
 
+const jsnack5Element = document.getElementById('jsnack-5');
+
 const arrayOne = [1,2,3,4,5,6,7,8,9,10];
 const arrayTwo = [1,2,3,4,5];
 
@@ -30,3 +32,23 @@ while(smallerArray.length < biggerArray.length)
 
 console.table(arrayOne);
 console.table(arrayTwo);
+let message = '';
+
+for(i = 0 ; i < arrayOne.length ; i++)
+{
+    if(!i)
+    {
+        message += "ARRAY UNO <br>"
+    }
+    message += arrayOne[i] + '<br>' 
+}
+for(i = 0 ; i < arrayTwo.length ; i++)
+{
+    if(!i)
+    {
+        message += "ARRAY DUE <br>"
+    }
+    message += arrayTwo[i] + '<br>' 
+}
+
+jsnack5Element.innerHTML = message; 
